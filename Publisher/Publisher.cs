ebf79586bf0e9c29f8a16c6144a316f2be1ec9ce
Steven Suing <Steven.suing@gmail.com>
2013-08-28 09:12:15 -0400

    Initial Commit

---
+++ Publisher/Publisher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    public class Producer : IDisposable
+    {
+
+        protected IModel Model;
+        protected IConnection Connection;
+        protected string QueueName;
+
+        public Producer(string hostName, string queueName)
+        {
+            QueueName = queueName;
+            var connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName = hostName;
+            Connection = connectionFactory.CreateConnection();
+            Model = Connection.CreateModel();
+            Model.QueueDeclare(QueueName, false, false, false, null);
+        }
+
+        public void SendMessage(byte[] message)
+        {
+            IBasicProperties basicProperties = Model.CreateBasicProperties();
+            Model.BasicPublish("", QueueName, basicProperties, message);
+        }
+        public void Dispose()
+        {
+            if (Connection != null)
+                Connection.Close();
+            if (Model != null)
+                Model.Abort();
+        }
+    }
+}
---
+++ RabbitMQHelloWorld/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Publisher;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using Subsciber;
+
+namespace RabbitMQHelloWorld
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public string HOST_NAME = "localhost";
+        public string QUEUE_NAME = "helloWorld";
+
+        private Consumer consumer;
+        private Producer producer;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            producer = new Producer(HOST_NAME, QUEUE_NAME);
+            producer.SendMessage(System.Text.Encoding.UTF8.GetBytes("Hello World"));
+        }
+
+        [TestMethod]
+        public void GetMessage()
+        {
+            //create the consumer
+            consumer = new Consumer(HOST_NAME, QUEUE_NAME);
+
+            //listen for message events
+            consumer.onMessageReceived += handleMessage;
+
+            //start consuming
+            consumer.StartConsuming();
+        }
+
+        //delegate to post to UI thread
+     //   private delegate void showMessageDelegate(string message);
+
+        //Callback for message receive
+        public void handleMessage(byte[] message)
+        {
+            Console.WriteLine(System.Text.Encoding.UTF8.GetString(message));
+           // showMessageDelegate s = new showMessageDelegate(richTextBox1.AppendText);
+
+          //  this.Invoke(s, System.Text.Encoding.UTF8.GetString(message) + Environment.NewLine);
+        }
+
+
+    }
+}
