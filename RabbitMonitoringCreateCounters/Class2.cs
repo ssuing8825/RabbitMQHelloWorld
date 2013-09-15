c36136626240ec3cf3111a335c136eb7138c458f
Steven Suing <Steven.suing@gmail.com>
2013-09-15 08:05:51 -0400

    Added PS COmmands

---
+++ RabbitMonitoringCreateCounters/Class2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMonitoringCreateCounters
+{
+    using System;
+    using System.Management.Automation;
+    
+    [Cmdlet(VerbsLifecycle.Install, "NServiceBusPerformanceCounters", SupportsShouldProcess = true, ConfirmImpact = ConfirmImpact.Medium)]
+    public class InstallPerformanceCounters : CmdletBase
+    {
+        protected override void ProcessRecord()
+        {
+            if (ShouldProcess(Environment.MachineName))
+            {
+                PerformanceCounterSetup.SetupCounters();
+            }
+        }
+    }
+
+    [Cmdlet(VerbsDiagnostic.Test, "NServiceBusPerformanceCountersInstallation")]
+    public class ValidatePerformanceCounters : CmdletBase
+    {
+        protected override void ProcessRecord()
+        {
+            var countersAreGood = PerformanceCounterSetup.CheckCounters();
+
+            WriteVerbose(countersAreGood
+                             ? "NServiceBus Performance Counters are setup and ready for use with NServiceBus."
+                             : "NServiceBus Performance Counters are not properly configured.");
+
+            WriteObject(countersAreGood);
+        }
+    }
+}
---
+++ RabbitMonitoringCreateCounters/CmdletBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Principal;
+
+namespace RabbitMonitoringCreateCounters
+{
+    using System.Management.Automation;
+    using System.Security;
+  
+    public abstract class CmdletBase : PSCmdlet
+    {
+        protected override void BeginProcessing()
+        {
+            if (!ProcessUtil.IsRunningWithElevatedPriviliges())
+            {
+                ThrowTerminatingError(new ErrorRecord(new SecurityException("You need to run this command with administrative rights."), "NotAuthorized", ErrorCategory.SecurityError, null));
+            }
+        }
+    }
+
+    public static class ProcessUtil
+    {
+        public static bool IsRunningWithElevatedPriviliges()
+        {
+            return new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+}
